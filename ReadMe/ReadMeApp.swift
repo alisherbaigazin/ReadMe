8fafe34f0133395036e33da345af832a4008be3e
alisherbaigazin <alisherbaigazin@gmail.com>
2022-05-27 03:05:44 +0600

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Alisher Baigazin on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
